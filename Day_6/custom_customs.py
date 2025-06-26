f3f1fdbdaea1a828862873d2b9af681a756bb061
yanti.lanning <yanti.lanning@virginmediao2.co.uk>
2025-06-26 12:43:33 +0100

    day 6

---
+++ Day_6/custom_customs.py
@@ -0,0 +1,27 @@
+from puzzle_input import inputs
+
+
+
+def count_yes_answers(answers:str) -> int:
+    # count the total number of unique 'yes' answers for all groups
+
+    groups = answers.strip().split('\n\n')
+    all_count = 0
+
+    for group in groups:
+        # Get all unique letters from the group
+        unique_answers = set()
+
+        # check each person's answer in the group
+        for person in group.split('\n'):
+            unique_answers.update(person.strip())
+
+        # count unique_answers for each group
+        group_count = len(unique_answers)
+        all_count += group_count
+
+    return all_count
+
+
+result = count_yes_answers(inputs)
+print(result)
