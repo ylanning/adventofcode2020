4d11647f159df38c09146e48af8fc1e5ddaf49e5
yanti.lanning <yanti.lanning@virginmediao2.co.uk>
2025-06-26 11:06:15 +0100

    day 4

---
+++ Day_4/count_valid_passports.py
@@ -0,0 +1,31 @@
+from passport_data import datas
+
+
+def count_valid_passports(passports=datas):
+
+    # split input string into individual passport block line
+    passport_lists = passports.strip().split('\n\n')
+    # print(passport_lists)
+
+    required_fields = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'}
+    valid_passports = 0
+
+    for passport in passport_lists:
+        fields = set()
+        # replace \n with a single space
+        items = passport.replace('\n', ' ').split()
+        # ['ecl:gry', 'pid:860033327', 'eyr:2020', 'hcl:#fffffd', 'byr:1937', 'iyr:2017', 'cid:147', 'hgt:183cm']
+
+        for item in items:
+            # split into a list and take the first element
+            field = item.split(':')[0]
+            fields.add(field)
+
+        # check if required_fields is existing in fields
+        if required_fields.issubset(fields):
+            valid_passports += 1
+
+    return valid_passports
+
+
+print(count_valid_passports(datas))
